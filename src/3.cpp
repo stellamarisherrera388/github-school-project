91d05220cf3937c08d82ef7d6969cd82ffa802cc
stellamarisherrera388 <stellamarisherrera388@gmail.com>
2025-03-08 05:51:58 +0000

    Add 3.cpp

---
+++ src/3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5;
+    if (x == 5) {
+        cout << "Hello World!" << endl;
+    }
+    else {
+        cout << "Goodbye World!" << endl;
+    }
+    return 0;
+}
