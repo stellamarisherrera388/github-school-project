7f4408311ab90b0da78faa6bbf420b6759416a90
stellamarisherrera388 <stellamarisherrera388@gmail.com>
2025-04-05 05:07:56 +0000

    Add 23.cpp

---
+++ src/23.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    if (age >= 18) {
+        cout << "Adult" << endl;
+    } else {
+        cout << "Minor" << endl;
+    }
+    return 0;
+}
