94010d2bd916832fdf7a18ffac49a40b194ad73b
stellamarisherrera388 <stellamarisherrera388@gmail.com>
2025-04-11 14:07:05 +0000

    Add 49.cpp

---
+++ src/49.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "X is positive." << endl;
+    } else {
+        cout << "X is not positive." << endl;
+    }
+    return 0;
+}
