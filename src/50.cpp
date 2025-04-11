77749f3f6ab070256926f986b78486c59ed2061c
stellamarisherrera388 <stellamarisherrera388@gmail.com>
2025-04-11 14:09:07 +0000

    Add 50.cpp

---
+++ src/50.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "Positive number" << endl;
+    } else {
+        cout << "Negative number" << endl;
+    }
+    return 0;
+}
