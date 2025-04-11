bd6220737ca61d3eda92937da52e719af7212337
stellamarisherrera388 <stellamarisherrera388@gmail.com>
2025-04-11 12:50:22 +0000

    Add 41.cpp

---
+++ src/41.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height = 10;
+    double width = 5.5;
+    cout << "The area of the rectangle is: " << height * width << endl;
+    return 0;
+}
