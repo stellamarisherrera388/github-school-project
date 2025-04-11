f54e95fd020303bb5ab9eb8b8c59d42c26516e36
stellamarisherrera388 <stellamarisherrera388@gmail.com>
2025-04-11 11:09:04 +0000

    Add 27.cpp

---
+++ src/27.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.141592653589793;
+    float z = 0.0f;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
+
+    return 0;
+}
